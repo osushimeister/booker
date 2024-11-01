e1cf87d4d9128533d7da598d35bfc45a9d9510d0
osushimeister <suomi.zu@gmail.com>
2024-11-01 07:13:43 +0000

    [finish] indexにshowを表示

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,19 +1,20 @@
 class BooksController < ApplicationController
   def index
-    @book=Book.new
-    @books=Book.all
+    @book = Book.new
+    @books = Book.all
   end
 
   def edit
   end
 
   def create
-    book=Book.new(book_params)
+    book = Book.new(book_params)
     book.save
     redirect_to '/books'
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   private
