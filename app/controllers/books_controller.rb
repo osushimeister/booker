bde70f6c8da9a8d1a1db245789a8e1ec7616f741
osushimeister <suomi.zu@gmail.com>
2024-11-01 06:26:45 +0000

    [update] 余計に生成していたviewファイルを削除

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,22 @@
 class BooksController < ApplicationController
   def index
+    @book=Book.new
   end
 
   def edit
   end
 
+  def create
+    book=Book.new(book_params)
+    book.save
+    redirect_to '/books'
+  end
+
   def show
   end
 
-  def new
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
